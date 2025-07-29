fc4f9e0ce913eda0920d8fb0da7a215bff8b1f30
Leapfrog <leapfrog@LF-00002109.local>
2025-07-29 12:44:35 +0545

    Update FE test files

--- frontend/src/components/UserList.js
+++ frontend/src/components/UserList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { act } from 'react';
 
 const UserList = ({ users, onUserDeleted }) => {
   const handleDelete = (user) => {
--- frontend/src/tests/unit/UserList.test.js
+++ frontend/src/tests/unit/UserList.test.js
@@ -1,4 +1,5 @@
-import { act, render, screen, fireEvent } from '@testing-library/react';
+import { act } from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import UserList from '../../components/UserList';
 
