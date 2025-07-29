cda363fc943665e7753b66d311c078097b822f8e
Leapfrog <leapfrog@LF-00002109.local>
2025-07-29 12:38:25 +0545

    Update FE test files

--- frontend/src/tests/unit/UserForm.test.js
+++ frontend/src/tests/unit/UserForm.test.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { act, render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import UserForm from '../../components/UserForm';
 
