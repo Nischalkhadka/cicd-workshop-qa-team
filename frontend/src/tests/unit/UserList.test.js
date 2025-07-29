41e29c301209a367ced5b3bb25c99e10a43ac22c
Leapfrog <leapfrog@LF-00002109.local>
2025-07-29 12:18:53 +0545

    Update test file

--- frontend/src/tests/unit/UserList.test.js
+++ frontend/src/tests/unit/UserList.test.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import {act} from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import UserList from '../../components/UserList';
